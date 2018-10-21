86ff396ee774ebbe66c3091bf857372e3459aa7f
Joel Uzcategui Lorenzo <Joel Uzcategui Lorenzo>
2018-10-21 09:55:19 -0400

    Primera prueba heroku

--- request.php
+++ request.php
@@ -34,26 +34,13 @@ $command = $arr[0];
  
 $message = substr(strstr($message," "), 1);
  
-//No requieren variables del usuario.
+// //No requieren variables del usuario.
 switch ($command) {
-    case '/ayuda':
+    case '/calculate':
         $response = "Tranquilo, estoy contigo.";
         sendMessage($chatId, $response);
         break;
-    case '/ayuda2':
-        $response = "Tranquilo, estoy contigo.";
-        $keyboard = '["Gracias"],["Pos Ok"]';
-        sendMessage($chatId, $response,$keyboard);
-        break;
-    case '/noticias':
-        getNoticias($chatId);
-        break;
-    case '/participar': case '/participar@VazCell_bot':
-        getSorteos($chatId, $message, $userId, $firstname, $agg);
-        break;
-    case '/youtube':
-        sendMessage($chatId, "Mi canal de YouTube es <a href='https://www.youtube.com/channel/UCGArCE3vmQkFpu_o_6axt1g'>SrVazquez</a>");
-    break;
+
  
 }
  
@@ -67,27 +54,27 @@ function sendMessage($chatId, $response, $keyboard = NULL){
     file_get_contents($url);
 }
  
-function getNoticias($chatId){
+// function getNoticias($chatId){
  
-    //include("simple_html_dom.php");
+//     //include("simple_html_dom.php");
  
-    $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
-    $url = "http://www.europapress.es/rss/rss.aspx";
+//     $context = stream_context_create(array('http' =>  array('header' => 'Accept: application/xml')));
+//     $url = "http://www.europapress.es/rss/rss.aspx";
  
-    $xmlstring = file_get_contents($url, false, $context);
+//     $xmlstring = file_get_contents($url, false, $context);
  
-    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
-    $json = json_encode($xml);
-    $array = json_decode($json, TRUE);
+//     $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+//     $json = json_encode($xml);
+//     $array = json_decode($json, TRUE);
  
-    for ($i=0; $i < 9; $i++) {
-        $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
-    }
+//     for ($i=0; $i < 9; $i++) {
+//         $titulos = $titulos."\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'> +info</a>";
+//     }
  
-    sendMessage($chatId, $titulos);
+//     sendMessage($chatId, $titulos);
  
  
  
-}
+// }
  
 ?>
